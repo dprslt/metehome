13dc77dd67a03bf03d76162a196cde7697577d6b
theod <theo.depresle@gmail.com>
2017-11-11 19:50:06 +0100

    Adding led coloring

--- led.py
+++ led.py
@@ -1,17 +1,60 @@
-import
+#!/usr/python
+# -*- coding: utf-8-*-
+
+from neopixel import *
+import numpy as np
+
+def compute_gradient_descent(elevation):
+    max = 10
+    min = -10
+
+    if elevation > max:
+        return 0
+    elif elevation < min:
+        return 1
+    else:
+        return 1 - (elevation + max) / (max - min)
 
 class led(object):
 
+    DARK_BLUE = np.array((0,0,120))
+    BLUE = np.array((0,150,255))
+
+    YELLOW = np.array((255,255,160))
+    ORANGE = np.array((255,120,0))
+
     """docstring for ."""
     def __init__(self, x, y,strip, strip_addr):
-        super(, self).__init__()
         self.x = x
         self.y = y
         self.strip = strip
         self.strip_addr = strip_addr
         self.color = Color(255,255,255)
 
-    def computeColor(sun_gradient_matrix):
+        self.blue_delta = self.BLUE - self.DARK_BLUE
+        self.yellow_delta = self.YELLOW - self.ORANGE
 
+
+    def computeColor(self, elevation, sun_gradient_matrix):
+
+        #print "\nid : ", self.strip_addr," x : ", self.x, " y : ",self.y
+        # Calcule la valeur de bleue et de jaune en fonction de l'elevation
+        gradient_descent = compute_gradient_descent(elevation)
+
+        sky_color = (self.BLUE - self.blue_delta * gradient_descent).astype(int)
+        sun_color = (self.YELLOW - self.yellow_delta * gradient_descent).astype(int)
+        #print "gradient_coeff : ",gradient_descent, " Elevation : ",elevation, " - sky : ", sky_color, " - sun : ", sun_color
+        # Calcule du dégradé entre le jaune et le bleu précédent
+
+        delta_sun_sky = sky_color - sun_color
+        led_color = sky_color - delta_sun_sky * sun_gradient_matrix[self.y][self.x]
+        #print "Delta : ",delta_sun_sky," - sun : ", sun_gradient_matrix[self.y][self.y]," - color : ",led_color
+
+        self.color = led_color
+        return;
+
+    def applyColorToMatrix(self, matrix):
+        matrix[self.y][self.x] = self.color
 
     def applyColorToStrip():
+        return;
--- metehome.py
+++ metehome.py
@@ -50,6 +50,10 @@ import sunpos2 as sunpos
 import math
 import time
 
+from PIL import Image
+
+from led import led
+
 
 ###
 if __name__ == '__main__':
@@ -62,18 +66,58 @@ if __name__ == '__main__':
     azimut_maison = 0
     lat_maison = 41
     long_maison = 0
-    # Positionnement des leds : 1 pour une led, 0 pour rien
-    size_y, size_x = (21,11)
+
+    # Dimensions en nombres de leds
+    longueur_maison = 75
+    largeur_maison  = 75
+
+    diffusion_soleil = 6
+
+    gradient_upscaling = 1.04
+
     ############################ Constantes ####################################
     # Lorsque l'orientation du soleil passe en dessous de cette limite,
     # c'est le debut de la nuit noire
     # Cette valeur sert à créer un dégradé pour la tombée de la nuit
-    LIMITE_COUCHER_DE_SOLEIL = 10
+    LIMITE_COUCHER_DE_SOLEIL = 20
     VALEUR_DE_NORMALISATION_DE_LA_MATRICE = 255
 
     ############################ Traitement ####################################
 
     steps = 0
+
+    leds = []
+    # Simulutation d'une piece carrée
+
+
+    ### Generation des leds
+    for x in range(largeur_maison):
+        led_x = x + int(largeur_maison*(gradient_upscaling-1)/2)
+
+        led_y_1 = int(longueur_maison*(gradient_upscaling-1)/2)
+        led_y_2 = longueur_maison + int(longueur_maison*(gradient_upscaling-1)/2)
+        led_y_3 = int(longueur_maison*(gradient_upscaling)/2)
+
+        leds.append(led(led_x,led_y_1,"","TODO"))
+        leds.append(led(led_x,led_y_2,"","TODO"))
+        leds.append(led(led_x,led_y_3,"","TODO"))
+
+
+    for y in range(longueur_maison):
+        led_y = y + int(longueur_maison*(gradient_upscaling-1)/2)
+
+        led_x_1 = int(largeur_maison*(gradient_upscaling-1)/2)
+        led_x_2 = largeur_maison + int(largeur_maison*(gradient_upscaling-1)/2)
+        led_x_3 = int(largeur_maison*(gradient_upscaling)/2)
+
+        leds.append(led(led_x_1,led_y,"","TODO"))
+        leds.append(led(led_x_2,led_y,"","TODO"))
+        leds.append(led(led_x_3,led_y,"","TODO"))
+
+    #####
+    size_y, size_x = (int(largeur_maison*gradient_upscaling),int(longueur_maison*gradient_upscaling))
+    print "Dimensions matrix_grad : (", size_y,",",size_x,")"
+
     while(True):
         # On calcul l'azimut et l'élévation du soleil en fonction de la date et
         # du lieu.
@@ -91,19 +135,29 @@ if __name__ == '__main__':
         # Diffusion de l'aura du soleil
         # Le facteur de 3.5 régule la force de la diffusion,
         # On l'a déterminé empiriquement
-        for i in range(int(max(size_x,size_y)*3.5)):
+        for i in range(int(max(size_x,size_y)*diffusion_soleil)):
             matrix_grad = diffusion(matrix_grad)
 
         # Normalisation des valeurs
-        matrix_grad = ((matrix_grad/matrix_grad.max()) * VALEUR_DE_NORMALISATION_DE_LA_MATRICE).astype(int)
+        matrix_grad = (matrix_grad/matrix_grad.max())
 
         # Si le soleil est couché, on met en place une descende graduelle de la luminosité
         if elevation < 0 :
             degrade = (LIMITE_COUCHER_DE_SOLEIL - min(-elevation, LIMITE_COUCHER_DE_SOLEIL)) / LIMITE_COUCHER_DE_SOLEIL
             matrix_grad = (matrix_grad * degrade).astype(int)
 
-        print "", matrix_grad
+        #exit()
 
-        steps += 1
-        print ""
-        time.sleep(0.01)
+        #print "", (matrix_grad*255).astype(int)
+
+
+        data = np.zeros((size_y,size_x,3), dtype=np.uint8)
+        for l in leds:
+            l.computeColor(elevation,matrix_grad)
+            l.applyColorToMatrix(data)
+        img = Image.fromarray(data,'RGB')
+
+        img.save('imgs/s%05d.png' % (steps))
+
+        steps += 5
+        #time.sleep(0.09)
